4214df55d6ebc4e83dde08f077e6d1cbf003e8d0
Eric Thayer <eric.thayer594@gmail.com>
2019-04-23 00:57:59 -0600

    postCSS config, initial layout styles, browserSync setup

--- laravel/resources/views/welcome.blade.php
+++ laravel/resources/views/welcome.blade.php
@@ -1,16 +1,18 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+      <meta name="csrf-token" content="<?php echo csrf_token(); ?>">
+      <title>OOP Donuts :: </title>
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="css/app.css">
+
+      <!-- Styles -->
+      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
+        <div class="content-wrap flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
@@ -25,24 +27,24 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    PHP Rocks!
-                </div>
+            <main class="content">
+                <h1 class="title">OOP Donuts</h1>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                  <a href="https://laravel.com/docs">Docs</a>
+                  <a href="https://laracasts.com">Laracasts</a>
+                  <a href="https://laravel-news.com">News</a>
+                  <a href="https://blog.laravel.com">Blog</a>
+                  <a href="https://nova.laravel.com">Nova</a>
+                  <a href="https://forge.laravel.com">Forge</a>
+                  <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
-            </div>
+              </main>
+
+            <div class="image-wrap"></div>
         </div>
 
-        {{-- <script src="js/app.js"></script> --}}
+        {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
 
     </body>
 </html>
--- laravel/webpack.mix.js
+++ laravel/webpack.mix.js
@@ -11,5 +11,26 @@ const mix = require('laravel-mix');
  |
  */
 
+mix.options({
+  postCss: [
+    require('autoprefixer')({
+      browsers: ['last 2 versions', '> 1%', 'not ie < 10'],
+      cascade: false
+    }),
+    require('postcss-custom-properties'),
+  ],
+  purifyCss: true,
+});
+
+mix.browserSync({
+  proxy: 'local.donutsoop.com'
+});
+
+mix.copyDirectory('resources/media', 'public/media');
+
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.minify([
+  'public/js/app.js', 'public/css/app.css'
+]);
